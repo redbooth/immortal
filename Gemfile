a92d34fcda53bf89f71845540884676192652717
Victor Martinez <knoopx@gmail.com>
2013-02-25 12:24:19 +0100

    Added travis support

--- Gemfile
+++ Gemfile
@@ -1,15 +1,2 @@
 source "http://rubygems.org"
-
-# Specify your gem's dependencies in immortal.gemspec
 gemspec
-
-group :test, :development do
-
-  # gem "ruby-debug", "~> 0.10.4", :require => "ruby-debug", :platform => :ruby_18
-
-  # gem "ruby-debug19", :require => "ruby-debug", :platform => :ruby_19
-  # # Uncomment this and rebundle to use the debugger under 1.9.3
-  # TODO: Remove this when ruby-debug-base19-0.11.26 is published
-  # gem "ruby-debug-base19", "0.11.26", :platform => :ruby_19
-
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,17 @@ require 'logger'
 
 require File.expand_path("../../lib/immortal", __FILE__)
 
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+connection_opts = case ENV.fetch('DB', "sqlite")
+                    when "sqlite"
+                      {adapter: "sqlite3", database: ":memory:"}
+                    when "mysql"
+                      {adapter: "mysql2", database: "immortal", username: "root", encoding: "utf8"}
+                    when "postgres"
+                      {adapter: "postgresql", database: "immortal", username: "postgres"}
+                  end
+
+ActiveRecord::Base.establish_connection(connection_opts)
+
 ActiveRecord::Base.logger = Logger.new(STDOUT) if ENV['DEBUG']
 silence_stream(STDOUT) { require 'support/schema' }
 
